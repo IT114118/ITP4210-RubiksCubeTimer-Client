69e9dea1db593593c9e808acad82968ceb409998
MrGGTo <mrggto@gmail.com>
2020-06-09 17:24:40 +0800

    updated - RecordModel.java - UserModel.java (With constructor, toString, getters and setters)

--- app/src/main/java/com/example/project4210/MainActivity.java
+++ app/src/main/java/com/example/project4210/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.project4210;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //id linking with xml
+        btn_timer = findViewById(R.id.btn_timer);
 
+        btn_timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,TimerActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/project4210/TimerActivity.java
+++ app/src/main/java/com/example/project4210/TimerActivity.java
@@ -2,12 +2,34 @@ package com.example.project4210;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class TimerActivity extends AppCompatActivity {
+
+    private String[] rotations = {
+            "U","D","R","L","F","B",
+            "U'","D'","R'","L'","F'",
+            "U2","D2","R2","L2","F2"
+    };
+
+    private TextView tv_scramble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        tv_scramble = findViewById(R.id.tv_scramble);
+        tv_scramble.setText(generateScramble(20));
+    }
+
+    private String generateScramble(int length){
+        String scramble = "";
+        for (int i = length; i <= length; i++) {
+            scramble += " " + rotations[(int)Math.random()*rotations.length].toString();
+        }
+        return scramble;
     }
 }
