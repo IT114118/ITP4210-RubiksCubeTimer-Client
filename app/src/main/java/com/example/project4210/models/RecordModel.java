491a0b2d5d7917cc8b8a6545fbbc23e46b6da3fb
MrGGTo <mrggto@gmail.com>
2020-06-09 16:47:53 +0800

    added - RecordModel.java - UserModel.java

---
+++ app/src/main/java/com/example/project4210/models/RecordModel.java
@@ -0,0 +1,9 @@
+package com.example.project4210.models;
+
+public class RecordModel {
+
+    private float time;
+    private String scramble;
+    private boolean isStarred;
+
+}
---
+++ app/src/main/java/com/example/project4210/models/UserModel.java
@@ -0,0 +1,14 @@
+package com.example.project4210.models;
+
+public class UserModel {
+
+    private int id;
+    private String name;
+    private String password;
+
+    private float personalBest;
+    private float ao5;
+    private float ao12;
+    private float average;
+
+}
